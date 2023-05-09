f800da9b50d180138212816f5dfb864a43f22ee4
Denys Maksymchuk <denysmaksimchuk@gmail.com>
2023-05-09 17:15:49 +0200

    Add hw-7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -4,23 +4,52 @@ import { galleryItems } from "./gallery-items.js";
 console.log(galleryItems);
 
 const gallery = document.querySelector(".gallery");
-
 const cardsMurkup = addMarkupToGellery(galleryItems);
-gallery.insertAdjacentHTML("beforebegin", cardsMurkup);
+
+gallery.addEventListener("click", onClickImg);
+gallery.insertAdjacentHTML("afterbegin", cardsMurkup);
 
 function addMarkupToGellery(galleryItems) {
   return galleryItems
     .map(({ preview, description, original }) => {
       return `<li class="gallery__item">
-        <a class="gallery__link" href="${original}">
-          <img
-            class="gallery__image"
+    <a class="gallery__link" href="${original}">
+    <img
+    class="gallery__image"
             src="${preview}"
            data-source="${original}"
-            alt="${description}"
-          />
-        </a>
-      </li>`;
+           alt="${description}"
+           />
+           </a>
+           </li>`;
     })
     .join("");
+}
+
+function onClickImg(e) {
+  e.preventDefault();
+
+  if (e.target.nodeName !== "IMG") {
+    retun;
+  }
+
+  const onCloseModal = (e) => {
+    const ESC_KEY = "Escape";
+    if (e.code === ESC_KEY) {
+      istance.close();
+    }
+  };
+
+  const istance = basicLightbox.create(
+    `<img src="${e.target.dataset.source}" width="800" height="600">`,
+    {
+      onShow: () => {
+        window.addEventListener("keydown", onCloseModal);
+      },
+      onClose: () => {
+        window.removeEventListener("keydown", onCloseModal);
+      },
+    }
+  );
+  istance.show();
 }
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,47 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+const cardsMurkup = addMarkupToGellery(galleryItems);
+
+gallery.addEventListener("click", onClickImg);
+gallery.insertAdjacentHTML("afterbegin", cardsMurkup);
+
+function addMarkupToGellery(galleryItems) {
+  return galleryItems
+    .map(({ preview, description, original }) => {
+      return `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`;
+    })
+    .join("");
+}
+
+function onClickImg(e) {
+  e.preventDefault();
+
+  if (e.target.nodeName !== "IMG") {
+    retun;
+  }
+
+  const onCloseModal = (e) => {
+    const ESC_KEY = "Escape";
+    if (e.code === ESC_KEY) {
+      linkGallery.close();
+    }
+  };
+
+  const linkGallery = new SimpleLightbox(".gallery a", {
+    captionsDelay: 250,
+
+    onShow: () => {
+      window.addEventListener("keydown", onCloseModal);
+    },
+    onClose: () => {
+      window.removeEventListener("keydown", onCloseModal);
+    },
+  });
+  linkGallery.show();
+}
